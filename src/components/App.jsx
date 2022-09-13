8c5d29e2e4dca7c01f04b36a507d1ac28166f694
Natalia <72099196+Nataliia2@users.noreply.github.com>
2022-09-13 21:30:14 +0300

    правка расширения

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,11 @@
-import user from './data/user';
+import user from './data/user.json';
 import { Profil } from './Profil/profil';
-import statistics from './data/statistics';
+import statistics from './data/statistics.json';
 import { Statistics } from './Statistics/Statistics';
-import friends from './data/friends';
+import friends from './data/friends.json';
 import { FriendList } from './FriendList/FriendList';
-import transactions from './data/transactions';
-import { TransactionHistory } from './TransactionHistory.jsx/TransactionHistory';
+import transactions from './data/transactions.json';
+import { TransactionHistory } from './TransactionHistory/TransactionHistory';
 
 
 export const App = () => {
